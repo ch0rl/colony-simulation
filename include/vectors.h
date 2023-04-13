e970df040be4524c2d3d14373473df5511948b82
Charlie Redpath <github@chorl.dev>
2023-04-13 19:45:21 +0100

    Use soley doubles for vectors

--- include/vectors.h
+++ include/vectors.h
@@ -20,38 +20,32 @@
 typedef unsigned int uint;
 
 typedef struct {
-    uint x;
-    uint y;
-} Vector2_uint;
-
-typedef struct {
-    int x;
-    int y;
-} Vector2_int;
-
-typedef struct {
-    float x;
-    float y;
-} Vector2_float;
+    double x;
+    double y;
+} Vector2;
 
 /*
- * random_vec
- * ----------
- * Generate a random (uint) vector.
+ * random_vec_uint
+ * ---------------
+ * Generate a random vector:
+ *  .x in [0, x_max]
+ *  .y in [0, y_max]
  */
-Vector2_uint random_vec_uint(uint x_max, uint y_max) {
-    return (Vector2_uint) {
-        .x = rand() % x_max,
-        .y = rand() % y_max
+Vector2 random_vec_uint(uint x_max, uint y_max) {
+    return (Vector2) {
+        .x = (double)rand_uint(x_max),
+        .y = (double)rand_uint(y_max)
     };
 }
 
 /*
- * random_vec_float
- * ----------------
- * Generate a random (float) vector.
+ * random_vec_int
+ * --------------
+ * Generate a random vector:
+ *  .x in [-x_max, x_max]
+ *  .y in [-y_max, y_max]
  */
-Vector2_int random_vec_int(uint x_max, uint y_max) {
+Vector2 random_vec_int(uint x_max, uint y_max) {
     int x_mod = 1, y_mod = 1;
     
     if (rand() & 1) {
@@ -62,9 +56,9 @@ Vector2_int random_vec_int(uint x_max, uint y_max) {
         y_mod = -1;
     }
 
-    return (Vector2_int) {
-        .x = x_mod * (rand() % x_max),
-        .y = y_mod * (rand() % y_max)
+    return (Vector2) {
+        .x = x_mod * (double)rand_uint(x_max),
+        .y = y_mod * (double)rand_uint(y_max)
     };
 }
 
@@ -74,21 +68,21 @@ Vector2_int random_vec_int(uint x_max, uint y_max) {
  * Get all points strictly within n units (not including pos).
  * (return will need freeing)
  */
-Vector2_uint *neighbour_points(Vector2_uint pos, uint n, uint *num) {
+Vector2 *neighbour_points(Vector2 pos, uint n, uint *num) {
     *num = 0;
     // https://en.wikipedia.org/wiki/Gauss_circle_problem
     uint num_points = PI * n * (n + 2 * sqrt(2));
     
-    Vector2_uint *points = (Vector2_uint *)malloc(sizeof(Vector2_uint) * num_points);
+    Vector2 *points = (Vector2 *)malloc(sizeof(Vector2) * num_points);
 
     // Inefficient but oh well
     // Assume circle at (0, 0), add pos to points within
     // If (x, y) in circle, so are (-x, y), (x, -y), and (-x, -y)
-    Vector2_uint p_mods[4] = {
-        (Vector2_uint){1, 1},
-        (Vector2_uint){-1, 1},
-        (Vector2_uint){1, -1},
-        (Vector2_uint){-1, -1}
+    Vector2 p_mods[4] = {
+        (Vector2){1, 1},
+        (Vector2){-1, 1},
+        (Vector2){1, -1},
+        (Vector2){-1, -1}
     };
     
     uint points_i = 0;
@@ -102,7 +96,7 @@ Vector2_uint *neighbour_points(Vector2_uint pos, uint n, uint *num) {
                 if (x != pos.x || y != pos.y) {
                     *num += 4;
                     for (uint p_i = 0; p_i < 4; p_i++) {
-                        points[points_i++] = (Vector2_uint){
+                        points[points_i++] = (Vector2){
                             .x = pos.x + x * p_mods[p_i].x,
                             .y = pos.y + y * p_mods[p_i].y
                         };
@@ -120,32 +114,8 @@ Vector2_uint *neighbour_points(Vector2_uint pos, uint n, uint *num) {
  * ----------
  * Add two vectors.
  */
-Vector2_uint vector_uint_add(Vector2_uint a, Vector2_uint b) {
-    return (Vector2_uint) {
-        .x = a.x + b.x,
-        .y = a.y + b.y
-    };
-}
-
-/*
- * vector_add_int
- * --------------
- * Add two vectors.
- */
-Vector2_int vector_int_add(Vector2_int a, Vector2_int b) {
-    return (Vector2_int) {
-        .x = a.x + b.x,
-        .y = a.y + b.y
-    };
-}
-
-/*
- * vector_add_int
- * --------------
- * Add two vectors.
- */
-Vector2_int vector_u_int_add(Vector2_int a, Vector2_uint b) {
-    return (Vector2_int) {
+Vector2 vector_add(Vector2 a, Vector2 b) {
+    return (Vector2) {
         .x = a.x + b.x,
         .y = a.y + b.y
     };
@@ -156,8 +126,8 @@ Vector2_int vector_u_int_add(Vector2_int a, Vector2_uint b) {
  * ----------
  * Subtract two vectors.
  */
-Vector2_int vector_uint_sub(Vector2_uint a, Vector2_uint b) {
-    return (Vector2_int) {
+Vector2 vector_sub(Vector2 a, Vector2 b) {
+    return (Vector2) {
         .x = a.x - b.x,
         .y = a.y - b.y
     };
@@ -168,32 +138,8 @@ Vector2_int vector_uint_sub(Vector2_uint a, Vector2_uint b) {
  * -------------------
  * Multiply a vector by a constant.
  */
-Vector2_uint vector_uint_times_k(Vector2_uint v, int k) {
-    return (Vector2_uint) {
-        .x = v.x * k,
-        .y = v.y * k
-    };
-}
-
-/*
- * vector_int_times_k
- * ------------------
- * Multiply a vector by a constant.
- */
-Vector2_int vector_int_times_k(Vector2_int v, int k) {
-    return (Vector2_int) {
-        .x = v.x * k,
-        .y = v.y * k
-    };
-}
-
-/*
- * vector_float_times_k
- * --------------------
- * Multiply a vector by a constant.
- */
-Vector2_float vector_float_times_k(Vector2_float v, int k) {
-    return (Vector2_float) {
+Vector2 vector_times_k(Vector2 v, double k) {
+    return (Vector2) {
         .x = v.x * k,
         .y = v.y * k
     };
@@ -204,34 +150,21 @@ Vector2_float vector_float_times_k(Vector2_float v, int k) {
  * ----------------
  * Subtract a constant from a vector.
  */
-Vector2_int vector_int_sub_k(Vector2_int v, uint k) {
-    return (Vector2_int) {
+Vector2 vector_sub_k(Vector2 v, double k) {
+    return (Vector2) {
         .x = v.x - k,
         .y = v.y - k
     };
 }
 
 /*
- * unit_vector_int
- * ---------------
+ * unit_vector
+ * -----------
  * Find the unit vector of v.
  */
-Vector2_float unit_vector_int(Vector2_int v) {
+Vector2 unit_vector(Vector2 v) {
     float size = sqrt(v.x * v.x + v.y * v.y);
-    return (Vector2_float) {
-        .x = (float)v.x / size,
-        .y = (float)v.y / size
-    };
-}
-
-/*
- * unit_vector_uint
- * ----------------
- * Find the unit vector of v.
- */
-Vector2_float unit_vector_uint(Vector2_uint v) {
-    float size = sqrt(v.x * v.x + v.y * v.y);
-    return (Vector2_float) {
+    return (Vector2) {
         .x = (float)v.x / size,
         .y = (float)v.y / size
     };
@@ -242,10 +175,10 @@ Vector2_float unit_vector_uint(Vector2_uint v) {
  * ------------
  * Round a vector's elements.
  */
-Vector2_uint vector_round(Vector2_float v) {
-    return (Vector2_uint) {
-        .x = (uint)round(v.x),
-        .y = (uint)round(v.y)
+Vector2 vector_round(Vector2 v) {
+    return (Vector2) {
+        .x = round(v.x),
+        .y = round(v.y)
     };
 }
 
